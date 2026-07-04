9c34cbc0a36c320ca27cccdd532b044562523e97
Danila Nenashev <danil.nenashev1337@gmail.com>
2026-07-04 12:48:05 +0400

    feature/statistics

--- internal/core/domain/task.go
+++ internal/core/domain/task.go
@@ -53,6 +53,20 @@ func NewTaskUnitialized(title string, description *string, authorUserID int) Tas
 	)
 }
 
+func (t *Task) CompletionDuration() *time.Duration {
+	if !t.Completed {
+		return nil
+	}
+
+	if t.CompletedAt == nil {
+		return nil
+	}
+
+	duration := t.CompletedAt.Sub(t.CreatedAt)
+
+	return &duration
+}
+
 func (t *Task) Validate() error {
 	titleLen := len([]rune(t.Title))
 	if titleLen < 1 || titleLen > 100 {
